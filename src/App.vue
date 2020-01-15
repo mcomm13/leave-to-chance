4e6dcd41ca87654f07336b4221ebb99496a0a243
Mike Commendatore <michael.commendatore@outlook.com>
2020-01-14 20:08:17 -0500

    True initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">Leave To Chance</div>
+      <v-spacer></v-spacer>
+      <v-btn href="https://github.com/mcomm13/leave-to-chance" target="_blank" text>
+        <span class="mr-2">View on GitHub</span>
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <LeaveToChance />
+    </v-content>
+
+    <v-footer>
+      <div>&copy; 2020 Michael Commendatore</div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeaveToChance from "./components/LeaveToChance";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    LeaveToChance
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/LeaveToChance.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="leave-to-chance">
+    <!-- <v-content> -->
+    <v-container>
+      <h1>Use this app to help you leave a decision up to chance</h1>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6">
+          <v-card class="elevation-12">
+            <v-card-text>
+              <v-row>
+                <v-col>
+                  <strong>How many options are you choosing from?</strong>
+                </v-col>
+                <v-col>
+                  <v-text-field v-model="numOfOptions" label="Number of options" />
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-row v-if="randomPick">
+              <v-col class="result">
+                <div class="result__header">Your randomly selected option is:</div>
+                <div class="result__value">{{randomPick}}</div>
+              </v-col>
+            </v-row>
+            <v-card-actions>
+              <v-row align="center" justify="center">
+                <v-btn v-on:click="makeRandomPick" color="primary">Make Random Pick</v-btn>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- </v-content> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      randomPick: 0,
+      numOfOptions: 2
+    };
+  },
+  methods: {
+    makeRandomPick: function() {
+      this.numOfOptions = parseInt(this.numOfOptions);
+      this.randomPick = Math.floor(Math.random() * this.numOfOptions) + 1;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+  font-size: 24px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+.result {
+  text-align: center;
+  &__header {
+    font-size: 24px;
+  }
+  &__value {
+    font-size: 32px;
+    font-weight: bold;
+  }
+}
+</style>
